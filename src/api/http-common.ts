d707623251b4d36e771fffe42c27179b0c675f9c
Semen Kolesnikov <skolesnikov@codeitup.ru>
2022-10-08 02:30:20 +0300

    header fix

--- src/api/http-common.ts
+++ src/api/http-common.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 export default axios.create({
   baseURL: process.env.REACT_APP_API_ENDPOINT,
   headers: {
-    'Content-type': 'application/json',
+    'Content-Type': 'application/json',
     // Authorization: `Bearer ${process.env.REACT_APP_API_TOKEN}`,
     timeout: 20000
   }
