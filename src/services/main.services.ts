be1753586a4511254fe054adaaad4dc7fb66a654
Semen Kolesnikov <skolesnikov@codeitup.ru>
2022-10-09 08:39:47 +0300

    Пофиксил загрузку обложки

--- src/services/main.services.ts
+++ src/services/main.services.ts
@@ -1,8 +1,14 @@
 import axios from 'api/http-common'
 
 const post = async <T>(data: T, endpoint: string, formData?: boolean) => {
-  console.log(123);
-
+  // if (formData) {
+  //   const json = JSON.stringify(data);
+  //   const blob = new Blob([json], {
+  //     type: 'application/json'
+  //   });
+  //   const form = new FormData();
+  //   form.append("upload_files", blob);
+  // }
   return await axios.post<T>(`v1/${endpoint}/`, data, formData ? { headers: { "Content-Type": "multipart/form-data" } } : {})
 }
 
--- src/ui/components/ModalInfo.tsx
+++ src/ui/components/ModalInfo.tsx
@@ -8,40 +8,41 @@ type Props = {
 }
 
 const Wrapper = styled.div`
-    width: 525px;
-    height: 200px;
-
-    background-color: ${theme.palette.white};
-    border: 1px solid ${theme.palette.blueGray};
-    border-radius: 24px;
+  position: relative;
+  width: 100%;
+  height: 150px;
+  background-color: ${theme.palette.white};
+  border-radius: 24px;
 `
 
 const Text = styled.p`
-    position: absolute;
-    padding: 10px 30px;
+  position: absolute;
+  padding: 10px 30px;
+  margin-top: 16px;
 
-    font-family: 'Roboto';
-    font-size: 16px;
-    font-weight: 400;
+  font-family: 'Roboto';
+  font-size: 16px;
+  font-weight: 400;
 `
 
 const Close = styled.img`
-    cursor: pointer;
-    margin-top: 10px;
-    margin-left: 490px;
+  position: absolute;
+  cursor: pointer;
+  top: 15px;
+  right: 10px;
+  z-index: 1;
 `
 
 export const _ModalInfo: FC<Props> = ({ isVisible }) => {
-
   return (
     <Wrapper>
-        <Close src={krestik} alt='close' onClick={() => isVisible(false)} />
-      <Text>Для получения NFT нужно выполнить все ежедневные задания 5 дней подряд. 
-        После получения ваш NFT будет стоить n₽, но его стоимость можно будет увеличить
-         выполняя уже разблокированные еженедельные задания. Доступно n уровней улучшения NFT. 
-         Финальная версия NFT будет стоить n₽₽₽. Его можно продать и получить «Digital 
-         Ruble» – цифровую валюту, которую можно обменять в магазине.
-        </Text>
+      <Close src={krestik} alt="close" onClick={() => isVisible(false)} />
+      <Text>
+        Для получения NFT нужно выполнить все ежедневные задания 5 дней подряд. После получения ваш NFT будет стоить n₽,
+        но его стоимость можно будет увеличить выполняя уже разблокированные еженедельные задания. Доступно n уровней
+        улучшения NFT. Финальная версия NFT будет стоить n₽₽₽. Его можно продать и получить «Digital Ruble» – цифровую
+        валюту, которую можно обменять в магазине.
+      </Text>
     </Wrapper>
   )
 }
